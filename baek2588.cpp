89690311eeecf608a7c06431a9c41e825a4c1d32
c99a97 <c99a97@gmail.com>
2021-01-03 20:03:00 +0900

    code

---
+++ baek2588.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int A, B;
+	
+	scanf("%d %d", &A, &B);
+	printf("%d\n", A*(B%10));
+	printf("%d\n", A*((B%100)/10));
+	printf("%d\n", A*(B/100));
+	printf("%d\n", A*B);
+
+	return 0;
+}
